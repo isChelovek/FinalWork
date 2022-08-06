7765f7e6097545b8739db07cabd582363743392c
Nikolay <fufaiker@gmail.com>
2022-08-06 15:36:52 +0300

    Add printArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+
+bool start = true;
+ WriteLine("Введите число строк в массиве. Число строк должно быть больше 0 и меньше 2147483647. Для выхода введите 0");
+ Write("Введите число: ");
+ string caseSwitch = "start";
+ int number = 0;
+ string[] userArray = {""};
+
+ void printArray(string[] array)
+{
+    Write("[");
+     for (int i = 0; i < array.Length; i++)
+     {
+        Write(array[i]);
+        Write(i < array.Length-1 ? ", " : "");
+     }
+     Write("]");
+}
